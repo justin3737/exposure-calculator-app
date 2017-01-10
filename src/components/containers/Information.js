d2c79f5f3265f14a79174eebdd36349da8706c48
Justin <juns3700@gmail.com>
2017-01-11 07:58:28 +0800

    add info content

--- src/components/containers/Information.js
+++ src/components/containers/Information.js
@@ -11,6 +11,11 @@ export default class Information extends Component {
         return (
             <div className={classnames({'info-container':true, 'is-open': this.props.isShow})}>
                 <Icon name="i-close" onClick={this.props.onClose}/>
+                <div className="info-wrap info1">
+                    <div className="info-cover"></div>
+                    <h2>拍攝與計算</h2>
+                    <h3>請拍攝一張照片，並且把光圈、快門、ISO的數據輸入到APP內</h3>
+                </div>
             </div>
         );
     };
