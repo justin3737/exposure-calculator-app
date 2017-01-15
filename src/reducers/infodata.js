3d2eb4d47d253c0bc46cc78d83b50d5c20514b4b
Justin <juns3700@gmail.com>
2017-01-15 23:46:58 +0800

    modify wording

--- src/reducers/infodata.js
+++ src/reducers/infodata.js
@@ -1,29 +1,33 @@
 let infodata = {
     "ZH_TW":[
         {
-            "title": "拍一張照片",
-            "desc" : "請拍攝一張照片，並且把光圈、快門、ISO的數據輸入到APP內"
+            "title": "設計理念",
+            "desc" : "在拍攝長曝的照片時，需要利用高ISO跟大光圈構圖，接著換回低ISO小光圈長曝，因此這個APP就是用來解決換算問題"
         },
         {
-            "title": "按下LOCK",
-            "desc" : "接著，按下『 LOCK 』鎖定目前的曝光值"
+            "title": "輸入與鎖定",
+            "desc" : "請使用單眼相機拍攝一張照片，並且把光圈、快門、ISO的數據輸入到APP內，接著按下『 LOCK 』鎖定目前的曝光值"
         },
         {
             "title": "左右滑動",
-            "desc" : "最後，您可以左右滑動調整光圈或ISO，可以得到新的快門速度"
+            "desc" : "左右滑動調整光圈、ISO得到新的快門值，接著設定到單眼相機內"
         },
         {
             "title": "反覆操作",
-            "desc" : "按下『 UNLOCK 』可以重設新的曝光組合"
+            "desc" : "按下『 UNLOCK 』重複前面幾個步驟可以重設新的曝光值組合"
+        },
+        {
+            "title": "建立捷徑",
+            "desc" : "在手機瀏覽器裡，可以新增加入『 主畫面 』或『 桌面 』"
         }
     ],
     "EN_US":[
         {
-            "title": "Take Photo",
+            "title": "Introdction",
             "desc" : ""
         },
         {
-            "title": "Tap LOCK",
+            "title": "Input Exposure",
             "desc" : ""
         },
         {
@@ -32,6 +36,10 @@ let infodata = {
         },
         {
             "title": "Repeat",
+            "desc" : ""
+        },
+        {
+            "title": "Shortcut",
             "desc" : ""
         }
     ]
